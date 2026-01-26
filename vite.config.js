84422393871c2613215d332a8357acceaa59007c
Don <154335034+Steveston147@users.noreply.github.com>
2026-01-26 17:06:22 +0900

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/mitsumori/",
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
 });
