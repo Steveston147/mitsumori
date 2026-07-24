1fdce9e6f1478c8f9d56c6e7c783a8b93ba28014
Don <154335034+Steveston147@users.noreply.github.com>
2026-07-24 23:37:17 +0900

    Support Vercel and GitHub Pages base paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mitsumori/",
+  base: process.env.VERCEL ? "/" : "/mitsumori/",
   plugins: [react()],
 });
