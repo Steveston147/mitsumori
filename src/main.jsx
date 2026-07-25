8c32649d91b154d241492dceff639fcbe57df727
Don <154335034+Steveston147@users.noreply.github.com>
2026-07-25 15:14:50 +0900

    Render common program basic information

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ProgramBasicInfo from "./components/ProgramBasicInfo.jsx";
 import JapaneseCourseCosts from "./components/JapaneseCourseCosts.jsx";
 import StudentCollaborationCosts from "./components/StudentCollaborationCosts.jsx";
 import CommonCosts from "./components/CommonCosts.jsx";
@@ -10,6 +11,7 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ProgramBasicInfo />
     <JapaneseCourseCosts />
     <StudentCollaborationCosts />
     <CommonCosts />
